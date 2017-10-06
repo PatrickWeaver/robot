df0eb579042a6bb36928e2c05284977c07dbbeda
Patrick Weaver <pjpwea@gmail.com>
2017-10-06 16:01:07 -0400

    add sensor and turning

--- Robot.ino
+++ Robot.ino
@@ -1,89 +1,80 @@
-/*
- Fading
+int y = 2;
+int b = 4;
+int g = 7;
+int w = 8;
 
- This example shows how to fade an LED using the analogWrite() function.
-
- The circuit:
- * LED attached from digital pin 9 to ground.
-
- Created 1 Nov 2008
- By David A. Mellis
- modified 30 Aug 2011
- By Tom Igoe
-
- http://www.arduino.cc/en/Tutorial/Fading
-
- This example code is in the public domain.
-
- */
-
-
-int lR = 9;    // LED connected to digital pin 9
+int lR = 9;
 int lG = 10;
 int lB = 11;
+int lEye[3] = {lR, lG, lB};
 
 int rR = 3;
 int rG = 5;
 int rB = 6;
+int rEye[3] = {rR, rG, rB};
 
-int dir;
-int dt = 4000;
+int speaker = 13;
+
+int forwardSensor = 0;
+int rotation = 0; // 0 is Clockwise, 1 is CounterClockwise
+
+void cw(){
+  digitalWrite(g, HIGH);
+}
+
+void ccw(){
+  digitalWrite(b, HIGH);
+}
+
+void turn(int turnDirection){
+  cw();
+  delay(500);
+}
+
+void forwardish(){
+  digitalWrite(y, HIGH);
+  digitalWrite(b, HIGH);
+  digitalWrite(g, HIGH);
+  delay(250);
+}
+
+void off(){
+  digitalWrite(y, LOW);
+  digitalWrite(b, LOW);
+  digitalWrite(g, LOW);
+  digitalWrite(w, LOW);
+}
 
 void setup() {
-  // nothing happens in setup
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(rR, OUTPUT); // Pin 3
-  pinMode(4, OUTPUT);
-  pinMode(rG, OUTPUT); // Pin 5
-  pinMode(rB, OUTPUT); // Pin 6
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(lR, OUTPUT); // Pin 9
-  pinMode(lG, OUTPUT); // Pin 10
-  pinMode(lB, OUTPUT); // Pin 11
-  pinMode(12, INPUT_PULLUP);
-  pinMode(13, OUTPUT);
   Serial.begin(9600);
+  
+  pinMode(y, OUTPUT);      // Pin 2
+  pinMode(rR, OUTPUT);     // Pin 3
+  pinMode(b, OUTPUT);      // Pin 4
+  pinMode(rG, OUTPUT);     // Pin 5
+  pinMode(rB, OUTPUT);     // Pin 6
+  pinMode(g, OUTPUT);      // Pin 7
+  pinMode(w, OUTPUT);      // Pin 8
+  pinMode(lR, OUTPUT);     // Pin 9
+  pinMode(lG, OUTPUT);     // Pin 10
+  pinMode(lB, OUTPUT);     // Pin 11
+  pinMode(speaker, OUTPUT); // Pin 13
+
+  delay(2000);
 }
 
 void loop() {
-  Serial.println(digitalRead(12));
-  if (digitalRead(12) != 1) {
-    digitalWrite(4, HIGH);
-    digitalWrite(7, HIGH);
-    digitalWrite(rR, HIGH);
-    digitalWrite(rG, HIGH);
-    digitalWrite(lB, HIGH);
-    tone(13, 170, dt);
+  int distance = analogRead(forwardSensor);
+  
+  if (distance > 40) {
+    forwardish();
     
-    delay(dt);
-
-    digitalWrite(4, LOW);
-    digitalWrite(7, LOW);
-    digitalWrite(rG, LOW);
-    digitalWrite(lB, LOW);
+  } else {
+    turn(rotation);
+    
   }
   
-  if (digitalRead(12) != 1){
-    digitalWrite(2, HIGH);
-    digitalWrite(8, HIGH);
-    digitalWrite(lG, HIGH);
-    tone(13, 220, dt);
-    
-    delay(dt);
-    
-    digitalWrite(2, LOW);
-    digitalWrite(8, LOW);
-    digitalWrite(rR, LOW);
-    digitalWrite(lG, LOW);
-  }
-
-  for (int i= 0; i < 12; i++) {
-    digitalWrite(i, LOW);
-  }
-
-  delay(50);
+  off();
 }
 
 
